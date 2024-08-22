4d0b71b3825573d16d8fc7611569cbc22d5e4f5f
Gustavo Pavão <116770050+gustapavao@users.noreply.github.com>
2024-08-21 22:42:48 -0300

    Ajustes na responsividade e prevenção a task vazia (#3)

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,16 +2,22 @@ import React, {useState} from "react";
 
 export const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState("")
+    const [vazio, setVazio] = useState(false);
 
     const handleSubmit = e => {
         e.preventDefault();
-        addTodo(value)
-        setValue("")
-    }
+        if (value.trim() === "") {
+            setVazio(true);
+            return;
+        }
+        addTodo(value);
+        setValue("");
+        setVazio(false);
+    };
     return(
         <form className="todoForm" onSubmit={handleSubmit}>
-            <input type="text" className="todo-input" value={value} placeholder="Quais os objetivos de hoje?"
-            onChange={(e) => setValue(e.target.value)}/>
+            <input type="text" className={`todo-input ${vazio ? "input-vazio" : ""}`} value={value} placeholder="Quais os objetivos de hoje?"
+            onChange={(e) => {setValue(e.target.value); setVazio(false);}}/>
             <button type="submit" className="todo-btn">+</button>
         </form>
     )   
